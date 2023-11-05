af4db5d3f84e207ed4241c3f642bd04ffc7e473c
kelseyblanchard <73808546+kelseyblanchard@users.noreply.github.com>
2023-11-05 16:08:03 -0500

    Create pulseSensor.ino
    Juliana's PulseSensor code sending to realtime database on Firebase and Kelsey's additions to read in the realtime data from Firebase and sound of the active buzzer

---
+++ pulseSensor.ino
@@ -0,0 +1,247 @@
+// Send
+/*
+   Sketch to handle each sample read from a PulseSensor.
+   Typically used when you don't want to use interrupts
+   to read PulseSensor voltages.
+
+   Here is a link to the tutorial that discusses this code
+   https://pulsesensor.com/pages/getting-advanced
+
+   Copyright World Famous Electronics LLC - see LICENSE
+   Contributors:
+     Joel Murphy, https://pulsesensor.com
+     Yury Gitman, https://pulsesensor.com
+     Bradford Needham, @bneedhamia, https://bluepapertech.com
+
+   Licensed under the MIT License, a copy of which
+   should have been included with this software.
+
+   This software is not intended for medical use.
+*/
+
+/*
+   Every Sketch that uses the PulseSensor Playground must
+   define USE_ARDUINO_INTERRUPTS before including PulseSensorPlayground.h.
+   Here, #define USE_ARDUINO_INTERRUPTS false tells the library to
+   not use interrupts to read data from the PulseSensor.
+
+   If you want to use interrupts, simply change the line below
+   to read:
+     #define USE_ARDUINO_INTERRUPTS true
+
+   Set US_PS_INTERRUPTS to false if either
+   1) Your Arduino platform's interrupts aren't yet supported
+   by PulseSensor Playground, or
+   2) You don't wish to use interrupts because of the side effects.
+
+   NOTE: if US_PS_INTERRUPTS is false, your Sketch must
+   call pulse.sawNewSample() at least once every 2 milliseconds
+   to accurately read the PulseSensor signal.
+*/
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "ufdevice"
+#define WIFI_PASSWORD "gogators"
+
+// Firebase credentials
+#define FIREBASE_HOST "https://industry-monitoring-fe214-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "AIzaSyA1EDvdDO3WNLQWXRyYzSEjUkzMn10EFVY"
+
+#define USE_ARDUINO_INTERRUPTS false
+#include <PulseSensorPlayground.h>
+
+//Define Firebase Data object
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+bool signupOK = false;
+
+/*
+   The format of our output.
+
+   Set this to PROCESSING_VISUALIZER if you're going to run
+    the Processing Visualizer Sketch.
+    See https://github.com/WorldFamousElectronics/PulseSensor_Amped_Processing_Visualizer
+
+   Set this to SERIAL_PLOTTER if you're going to run
+    the Arduino IDE's Serial Plotter.
+*/
+const int OUTPUT_TYPE = SERIAL_PLOTTER;
+
+/*
+   Pinout:
+     PULSE_INPUT = Analog Input. Connected to the pulse sensor
+      purple (signal) wire.
+     PULSE_BLINK = digital Output. Connected to an LED (and 1K series resistor)
+      that will flash on each detected pulse.
+     PULSE_FADE = digital Output. PWM pin onnected to an LED (and 1K series resistor)
+      that will smoothly fade with each pulse.
+      NOTE: PULSE_FADE must be a pin that supports PWM. Do not use
+      pin 9 or 10, because those pins' PWM interferes with the sample timer.
+     THRESHOLD should be set higher than the PulseSensor signal idles
+      at when there is nothing touching it. The expected idle value
+      should be 512, which is 1/2 of the ADC range. To check the idle value
+      open a serial monitor and make note of the PulseSensor signal values
+      with nothing touching the sensor. THRESHOLD should be a value higher
+      than the range of idle noise by 25 to 50 or so. When the library
+      is finding heartbeats, the value is adjusted based on the pulse signal
+      waveform. THRESHOLD sets the default when there is no pulse present.
+      Adjust as neccesary.
+*/
+#define BUZZER_PIN 18 
+
+const int PULSE_INPUT = 36;
+const int PULSE_BLINK = 2;
+const int PULSE_FADE = 5;
+const int THRESHOLD = 2000;   // Adjust this number to avoid noise when idle
+
+/*
+   samplesUntilReport = the number of samples remaining to read
+   until we want to report a sample over the serial connection.
+
+   We want to report a sample value over the serial port
+   only once every 20 milliseconds (10 samples) to avoid
+   doing Serial output faster than the Arduino can send.
+*/
+byte samplesUntilReport;
+const byte SAMPLES_PER_SERIAL_SAMPLE = 10;
+
+/*
+   All the PulseSensor Playground functions.
+*/
+PulseSensorPlayground pulseSensor;
+
+void setup() {
+  /*
+     Use 115200 baud because that's what the Processing Sketch expects to read,
+     and because that speed provides about 11 bytes per millisecond.
+
+     If we used a slower baud rate, we'd likely write bytes faster than
+     they can be transmitted, which would mess up the timing
+     of readSensor() calls, which would make the pulse measurement
+     not work properly.
+  */
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  // Wifi setup
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = FIREBASE_AUTH;
+  /* Assign the RTDB URL (required) */
+  config.database_url = FIREBASE_HOST;
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+  
+
+  // Configure the PulseSensor manager.
+  pulseSensor.analogInput(PULSE_INPUT);
+  pulseSensor.blinkOnPulse(PULSE_BLINK);
+  pulseSensor.fadeOnPulse(PULSE_FADE);
+
+  pulseSensor.setSerial(Serial);
+  pulseSensor.setOutputType(OUTPUT_TYPE);
+  pulseSensor.setThreshold(THRESHOLD);
+
+  // Skip the first SAMPLES_PER_SERIAL_SAMPLE in the loop().
+  samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+
+  // Now that everything is ready, start reading the PulseSensor signal.
+  if (!pulseSensor.begin()) {
+    /*
+       PulseSensor initialization failed,
+       likely because our Arduino platform interrupts
+       aren't supported yet.
+
+       If your Sketch hangs here, try changing USE_PS_INTERRUPT to false.
+    */
+    for(;;) {
+      // Flash the led to show things didn't work.
+      digitalWrite(PULSE_BLINK, LOW);
+      delay(50); Serial.println('!');
+      digitalWrite(PULSE_BLINK, HIGH);
+      delay(50);
+    }
+  }
+}
+
+void loop() {
+  if (pulseSensor.sawNewSample() && Firebase.ready() && signupOK) {
+    if (--samplesUntilReport == (byte)0) {
+      samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+
+      if (pulseSensor.sawStartOfBeat()) {
+        // Calculate and print the BPM (beats per minute)
+        float bpm = pulseSensor.getBeatsPerMinute();
+        Serial.println(bpm);
+
+        if (Firebase.RTDB.setInt(&fbdo, "bpm2/int", bpm)){
+          //Serial.println("PASSED");
+          //Serial.println("PATH: " + fbdo.dataPath());
+          //Serial.println("TYPE: " + fbdo.dataType());
+        }
+        else {
+          //Serial.println("FAILED");
+          //Serial.println("REASON: " + fbdo.errorReason());
+        }
+      }
+    }
+  }
+
+
+  if (Firebase.RTDB.getInt(&fbdo, "bpm/int")) {
+      int bpm_data = fbdo.intData();
+      Serial.println(bpm_data);
+      if (bpm_data >= 80) 
+      {
+        digitalWrite(BUZZER_PIN, HIGH);
+      }
+      else
+      {
+        digitalWrite(BUZZER_PIN, LOW);
+      }
+    }
+
+    if (Firebase.RTDB.getInt(&fbdo, "bpm2/int")) {
+      int bpm2_data = fbdo.intData();
+      Serial.println(bpm2_data);
+      if (bpm2_data >= 80) 
+      {
+        digitalWrite(BUZZER_PIN, HIGH);
+      }
+      else
+      {
+        digitalWrite(BUZZER_PIN, LOW);
+      }
+    }
+}

1a86824faebab2c93af34864bbd366a107fcede5
victoriadealba <victoriadealba@ufl.edu>
2023-11-07 16:44:09 -0500

    ppm readings displayed from firebase

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -7,10 +7,12 @@ import React, {useEffect,useState} from 'react';
 function App() {
   const[bpm, setBpm] = useState(null);
   const[bpm2, setBpm2] = useState(null);
+  const [gasValue, setPpm] = useState(null);
 
   useEffect(() => {
     const bmpRef = ref(db, 'bpm/int');
     const bpmReff = ref(db,'bpm2/int');
+    const gasRef = ref(db, 'gasValue/int');
 
     onValue(bmpRef,(snapshot) => {
       const data = snapshot.val();
@@ -20,6 +22,10 @@ function App() {
       const data = snapshot.val();
       setBpm2(data);
     });
+    onValue(gasRef,(snapshot) => {
+      const data = snapshot.val();
+      setPpm(data);
+    });
   }, []);
 
 
@@ -28,6 +34,7 @@ function App() {
       <header className="App-header">
         <h1>Realtime BPM1: {bpm}</h1>
         <h1>Realtime BPM2: {bpm2} </h1>
+        <h1>Realtime PPM: {gasValue}</h1>
 
       </header>
     </div>
